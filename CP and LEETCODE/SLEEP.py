db38895407a352facb8020ea4408c5dfb3afcb64
mukul baghel <96166432+mukulbaghel@users.noreply.github.com>
2022-10-19 01:52:37 +0530

    Update SLEEP.py

--- CP and LEETCODE/SLEEP.py
+++ CP and LEETCODE/SLEEP.py
@@ -3,7 +3,7 @@
 # A person is said to be sleep deprived if he slept strictly less than 777 hours in a day.
 # Chef was only able to sleep XXX hours yesterday. Determine if he is sleep deprived or not.
 
-for _ in range(int(input())):
+for i in range(int(input())):
     X = int(input())
     if X<7:
         print("Yes")
